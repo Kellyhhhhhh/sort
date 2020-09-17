30efd7bceabfeea6f769a3b2d04ebef63f90d7a3
Kellyhhhhhh <295566164@qq.com>
2020-09-17 19:05:39 +0800

    sort

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number,i = 0;
+	cout << "please input how many numbers in the array."<<endl;
+	cin >> number;
+	int* p = new int[number];
+
+	for (i = 0; i < number; i++) {
+		cin >> p[i];
+	}
+
+	for (int j = 1; j < number; j++) {
+		int key = p[j];
+		i = j - 1;
+		while (i > -1 && p[i] > key) {      //注重i范围
+			p[i + 1] = p[i];
+			i--;
+		}
+		p[i + 1] = key;
+	}
+
+	for (i = 0; i < number; i++) {
+		cout << p[i]<<" ";
+	}
+
+
+}
+
